21450605b30298c4814378af9530831e1cfa1fd9
Alexander Weber <alex.weber@nexteraanalytics.com>
2023-01-14 13:55:38 -0600

    doom

--- setup.sh
+++ setup.sh
@@ -39,6 +39,9 @@ case $1 in
     rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/* || true
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+
+    git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+    ~/.emacs.d/bin/doom install
     ;;
   link)
     rm $HOME/.zshrc || true
